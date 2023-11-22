7b628dbf052d3454a3700b7f986cca23384baa64
rakib <rakibronicse@gmail.com>
2023-11-23 00:50:04 +0100

    cart

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom';
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { ShopContext } from '../../Context/ShopContext';
 
 export const Navbar = () => {
   const [menu,setMenu] = useState('shop');
+  const {getTotalCartItems} = useContext(ShopContext);
+  
   return (
     <div className='navbar'>
       <div className="nav-logo">
@@ -23,7 +26,7 @@ export const Navbar = () => {
         <Link to='/cart'>
         <img src={cart_icon} alt="" />
         </Link>
-        <div className='nav-cart-count'>0</div>
+        <div className='nav-cart-count'>{getTotalCartItems()}</div>
       </div>
 
     </div>
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -14,8 +14,37 @@ const getDefaultCart = () =>{
 const ShopContextProvider = (props) => {
     
     const [cartItems,setCartItems] = useState(getDefaultCart());
-    const contextValue = {all_product,cartItems};
-    console.log(cartItems);
+
+    const addToCart = (itemId) =>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));  
+    }
+    
+    const removeFromCart = (itemId) =>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1})); 
+    } 
+
+    const getTotalCartAmount = () =>{
+        let totalAmount = 0;
+        for(const item in cartItems){
+            if(cartItems[item]>0){
+                let iteminfo = all_product.find((product)=>product.id === Number(item));
+                totalAmount += iteminfo.new_price * cartItems[item];
+            }           
+        } 
+        return totalAmount;
+    }
+
+    const getTotalCartItems = () =>{
+        let totalItem = 0;
+        for(const item in cartItems){
+            if(cartItems[item]>0){                
+                totalItem += cartItems[item];
+            }            
+        }
+        return totalItem;
+    }
+    const contextValue = {all_product,cartItems,addToCart,removeFromCart,getTotalCartAmount,getTotalCartItems};
+
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
